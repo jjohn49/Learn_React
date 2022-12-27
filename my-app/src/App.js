7f2a848bb906a3fa95cf2dce88b0d0b0811a5d96
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2022-12-27 11:53:54 -0500

    General push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <p>Lets see if this works</p>
+      <ExpenseItem />
     </div>
   );
 }
